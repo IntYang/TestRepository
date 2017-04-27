0d4c78332accc286f0fc33dc6ab4431669f6e284
IntYang <penguinyang01@gmail.com>
2017-04-27 18:06:50 +0900

    음량 슬라이더 추가 요망

---
+++ testApplication/src/RootController.java
@@ -0,0 +1,26 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Suyang
+ */
+public class RootController implements Initializable {
+
+	@FXML
+	private Button btn_call;
+	@FXML
+	private Button btn_show;
+	@FXML
+	private Button btn_open;
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO
+	}	
+	
+}
