5dbced5cb775875bbfbb2abdc13923a0804fbdad
IntYang <penguinyang01@gmail.com>
2017-05-23 16:21:47 +0900

    5/23 로거 설정

--- SpringWebProgramming/src/com/mycompany/myapp/controller/Exam01HomeController.java
+++ SpringWebProgramming/src/com/mycompany/myapp/controller/Exam01HomeController.java
@@ -1,16 +1,25 @@
 package com.mycompany.myapp.controller;
 
+
+// SLF4J 인터페이스를 공통 규격으로 규정하기 때문에 slf4j 것을 임포트 할 것
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Exam01HomeController {
+public class Exam01HomeController { 
+	// 로거가 되도록 설정
+	// static 객체마다 필드를 생성 안하게 하려고
+	// final 한번 값이 대입되면 안바뀌게 하려고
+	private static final Logger lOGGER = LoggerFactory.getLogger(Exam01HomeController.class);
 	
 	@RequestMapping("/")
 	public String home(){
-		System.out.println("home()");
+		lOGGER.info("/요청 처리함");
+		//System.out.println("home()");
 		return "home";
-		//앞에 WEB-INF/views		뒤에 .jsp 생략
+		//앞에 WEB-INF/views뒤에 .jsp 생략
 	}			
 	
 }
